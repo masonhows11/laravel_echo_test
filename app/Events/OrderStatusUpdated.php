38f8f1cc70c3b18be60a327c46d698f860311318
mason.hows11 <mason.hows11@gmail.com>
2025-11-29 19:36:14 +0330

    first commit

---
+++ app/Events/OrderStatusUpdated.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class OrderStatusUpdated implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $title;
+    /**
+     * Create a new event instance.
+     */
+    public function __construct($title)
+    {
+        //
+        $this->title = $title;
+
+         Log::info("TestEvent fired: " . $this->title);
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array
+     */
+    public function broadcastOn(): array
+    {
+        // this is custom chanel bro & public chanel
+        return [new Channel('order-status-updated')];
+        //        return [
+        //            new PrivateChannel('channel-name'),
+        //        ];
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Events\OrderStatusUpdated;
+use App\Livewire\Settings\Appearance;
+use App\Livewire\Settings\Password;
+use App\Livewire\Settings\Profile;
+use App\Livewire\Settings\TwoFactor;
+use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Features;
+
+Route::get('/', function () {
+
+    //    $ch = curl_init("https://google.com");
+    //    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    //    curl_exec($ch);
+    //    dd(curl_error($ch));
+
+    // dd(openssl_get_cert_locations());
+    // OrderStatusUpdated::dispatch();
+    event(new OrderStatusUpdated('hi'));
+
+    return view('welcome');
+})->name('home');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Route::get('settings/profile', Profile::class)->name('profile.edit');
+    Route::get('settings/password', Password::class)->name('user-password.edit');
+    Route::get('settings/appearance', Appearance::class)->name('appearance.edit');
+
+    Route::get('settings/two-factor', TwoFactor::class)
+        ->middleware(
+            when(
+                Features::canManageTwoFactorAuthentication()
+                && Features::optionEnabled(Features::twoFactorAuthentication(), 'confirmPassword'),
+                ['password.confirm'],
+                [],
+            ),
+        )
+        ->name('two-factor.show');
+});
