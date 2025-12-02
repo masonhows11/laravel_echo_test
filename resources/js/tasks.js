57c045fc9dce8d84b33fd1b8800ce1a872322279
mason.hows11 <mason.hows11@gmail.com>
2025-12-02 21:11:25 +0330

    problem solved

--- resources/js/tasks.js
+++ resources/js/tasks.js
@@ -1,47 +1,21 @@
 window.Echo.channel('tasks')
     .listen('.task.added', e => {
         console.log('new task added successfully');
-        console.log(e['task']);
         // Object.keys(e['task']) get keys of object from response
         // JSON.stringify(e['task']); convert to json string
         // Object.keys(e['task']).length
-
         // let response = JSON.stringify(e['task']);
         let response = e['task'];
-
         // let list = document.getElementById('task-list');
         let records = '';
-
-
-        let tr = document.createElement('tr');
-
-
-
-        for (const item in response) {
-            records += '<td class="p-2" colspan="2">' + response['id'] + '</td>' +
-                '<td class="p-2" colspan="2">' + response['title'] + '</td>' +
-                '<td class="p-2" colspan="2">' + response['body'] + '</td>' +
-                '<td class="p-2"><a href="#">edit</a></td>' +
-                '<td class="p-2"><a href="#">delete</a></td>';
-        }
-
-
-        // records += '<tr class="p-2 my-2 text-center" id="task-list">'
-        // for (const item in response) {
-        //     records += '<td class="p-2" colspan="2">' + response['id'] + '</td>' +
-        //         '<td class="p-2" colspan="2">' + response['title'] + '</td>' +
-        //         '<td class="p-2" colspan="2">' + response['body'] + '</td>' +
-        //         '<td class="p-2"><a href="#">edit</a></td>' +
-        //         '<td class="p-2"><a href="#">delete</a></td>';
-        // }
-        // records += '</tr>';
-
-
-        console.log(records);
-        // console.log(row)
-        // document.getElementById('table-list').append(row)
-        // tr.append(records);
-        document.getElementById('table-list').append(records);
-
-
+        records =
+            '<tr class="p-2 my-2 text-center" id="task-list">' +
+            '<td class="p-2" colspan="2">' + response['id'] + '</td>' +
+            '<td class="p-2" colspan="2">' + response['title'] + '</td>' +
+            '<td class="p-2" colspan="2">' + response['body'] + '</td>' +
+            '<td class="p-2"><a href="#">edit</a></td>' +
+            '<td class="p-2"><a href="#">delete</a></td>' +
+            '<tr/>'
+        // this is very important very_very important
+        document.getElementById('table-list').innerHTML += records;
     })
