9821d14b6156499f30d974cad3566b9af4b4b7fe
mason.hows11 <mason.hows11@gmail.com>
2025-12-02 18:44:13 +0330

    some thing goes work

--- resources/js/tasks.js
+++ resources/js/tasks.js
@@ -1,23 +1,28 @@
 window.Echo.channel('tasks')
     .listen('.task.added', e => {
         console.log('new task added successfully');
-        console.log(Object.keys(e['task']));
+        console.log(e['task']);
         // Object.keys(e['task']) get keys of object from response
         // JSON.stringify(e['task']); convert to json string
-        let response = JSON.stringify(e['task']);
-        let responseLength = response.length;
+        // Object.keys(e['task']).length
+
+        // let response = JSON.stringify(e['task']);
+        let response = e['task'];
+        let responseLength = Object.keys(e['task']).length;
+        //
         let list = document.getElementById('task-list');
         let records = '';
+        //
         let tr = document.createElement('tr');
         // console.log(response['id'],response['title'],response['body']);
-        for (let i = 0; i < responseLength; i++) {
-            records += '<td class="p-2" colspan="2">' + response[i]['id'] + '</td>';
-            records += '<td class="p-2" colspan="2">' + response[i]['title'] + '</td>';
-            records += '<td class="p-2" colspan="2">' + response[i]['body'] + '</td>';
+        for (const item in response) {
+            records += '<td class="p-2" colspan="2">' + response['id'] + '</td>';
+            records += '<td class="p-2" colspan="2">' + response['title'] + '</td>';
+            records += '<td class="p-2" colspan="2">' + response['body'] + '</td>';
             records += '<td class="p-2"><a href="#">edit</a></td>';
             records += '<td class="p-2"><a href="#">delete</a></td>';
         }
-        console.log(response);
+        console.log(records);
         //let node = list.appendChild(records);
         //document.getElementById('table-list').append(node)
 
