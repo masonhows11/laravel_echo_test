c32a745a5c24bad86fdaa8e4ff9aaa6e51e72fd5
mason.hows11 <mason.hows11@gmail.com>
2025-12-03 16:49:31 +0330

    add comment for private chat channel

--- routes/channels.php
+++ routes/channels.php
@@ -9,7 +9,9 @@ Broadcast::channel('tasks.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
-
+// parameter name in wildcard channel name
+// & parameter name in function must be same
+// otherwise error
 Broadcast::channel('chat.{roomId}',function($user,$roomId){
 
     return $user->rooms->where('id',$roomId)->exists();
