364f40b64f1b5ac5e887b4f46eb60c8bf88450e2
mason.hows11 <mason.hows11@gmail.com>
2025-12-05 08:17:33 +0330

    navbar items links & dashboard add home link updated

---
+++ resources/views/components/navbar-component.blade.php
@@ -0,0 +1,63 @@
+<nav class="flex flex-wrap items-center justify-between p-3 bg-teal-200/20">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+        class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round"
+            d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+    </svg>
+
+    <div class="flex md:hidden">
+        <button id="hamburger">
+            <img class="toggle block" src="https://img.icons8.com/fluent-systems-regular/2x/menu-squared-2.png"
+                width="48" height="48" />
+            <img class="toggle hidden" src="https://img.icons8.com/fluent-systems-regular/2x/close-window.png"
+                width="48" height="48" />
+        </button>
+    </div>
+    <div
+        class="toggle hidden w-full md:w-auto md:flex text-right text-bold mt-5 md:mt-0 border-t-2 border-teal-900 md:border-none">
+        @if (Route::has('login'))
+            @auth
+                <input type="hidden" id="user" value="{{ \Illuminate\Support\Facades\Auth::id() }}">
+                <a href="{{ route('home') }}"
+                    class="block md:inline-block text-teal-900 hover:text-teal-500 px-3 py-3 border-b-2 border-teal-900 md:border-none">
+                    Home
+                </a>
+                <a href="{{ url('/dashboard') }}"
+                    class="block md:inline-block text-teal-900 hover:text-teal-500 px-3 py-3 border-b-2 border-teal-900 md:border-none">
+                    Dashboard
+                </a>
+                <a href="{{ url('/tasks') }}"
+                    class="block md:inline-block text-teal-900 hover:text-teal-500 px-3 py-3 border-b-2 border-teal-900 md:border-none">Tasks
+                </a>
+                <a href="{{ url('/create') }}"
+                    class="block md:inline-block text-teal-900 hover:text-teal-500 px-3 py-3 border-b-2 border-teal-900 md:border-none">Create
+                </a>
+                <form method="post" action="{{ route('logout') }}">
+                    @csrf
+                    <button href="#" class="block md:inline-block text-teal-900
+                     hover:text-teal-500 px-3 py-3 border-b-2 border-teal-900 md:border-none">
+                       Logout
+                    </button>
+                </form>
+            @else
+                <a href="{{ route('login') }}"
+                    class="block md:inline-block text-teal-900 hover:text-teal-500 px-3 py-3 border-b-2 border-teal-900 md:border-none">Login
+                </a>
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}"
+                        class="block md:inline-block text-teal-900 hover:text-teal-500 px-3 py-3 border-b-2 border-teal-900 md:border-none">Contact
+                    </a>
+                @endif
+            @endauth
+        @endif
+    </div>
+    @guest
+        <a href="#"
+            class="toggle hidden md:flex w-full md:w-auto
+                     px-4 py-2 text-right bg-teal-900
+                    hover:bg-teal-500 text-white md:rounded">Create
+            Account
+        </a>
+    @endguest
+
+</nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,78 +1,35 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel Echo</title>
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
-        <meta name="csrf-token" content="{{ csrf_token()  }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-        <!-- Styles -->
-        <style>
-        </style>
-    </head>
-    <body class="flex  min-h-screen flex-col">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel Echo</title>
+    <link rel="icon" href="/favicon.ico" sizes="any">
+    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <nav class="flex flex-wrap items-center justify-between p-3 bg-teal-200/20">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-        </svg>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Styles -->
+    <style>
+    </style>
+</head>
 
-        <div class="flex md:hidden">
-            <button id="hamburger">
-                <img class="toggle block" src="https://img.icons8.com/fluent-systems-regular/2x/menu-squared-2.png"
-                     width="48" height="48"/>
-                <img class="toggle hidden" src="https://img.icons8.com/fluent-systems-regular/2x/close-window.png"
-                     width="48" height="48"/>
-            </button>
-        </div>
-        <div class="toggle hidden w-full md:w-auto md:flex text-right text-bold mt-5 md:mt-0 border-t-2 border-teal-900 md:border-none">
-            @if (Route::has('login'))
-                @auth
-                    <input type="hidden" id="user" value="{{ \Illuminate\Support\Facades\Auth::id() }}">
-                    <a href="{{ route('home') }}"
-                       class="block md:inline-block text-teal-900 hover:text-teal-500 px-3 py-3 border-b-2 border-teal-900 md:border-none">
-                        Home
-                    </a>
-                    <a href="{{ url('/dashboard') }}"
-                       class="block md:inline-block text-teal-900 hover:text-teal-500 px-3 py-3 border-b-2 border-teal-900 md:border-none">
-                        Dashboard
-                    </a>
-                    <a href="{{url('/tasks')}}"
-                       class="block md:inline-block text-teal-900 hover:text-teal-500 px-3 py-3 border-b-2 border-teal-900 md:border-none">Tasks
-                    </a>
-                    <a href="{{ url('/create')  }}"
-                       class="block md:inline-block text-teal-900 hover:text-teal-500 px-3 py-3 border-b-2 border-teal-900 md:border-none">Create
-                    </a>
-                @else
-                    <a href="{{ route('login')  }}"
-                       class="block md:inline-block text-teal-900 hover:text-teal-500 px-3 py-3 border-b-2 border-teal-900 md:border-none">Login
-                    </a>
-                    @if (Route::has('register'))
-                        <a href="{{ route('register')  }}"
-                           class="block md:inline-block text-teal-900 hover:text-teal-500 px-3 py-3 border-b-2 border-teal-900 md:border-none">Contact
-                        </a>
-                    @endif
-                @endauth
-            @endif
-        </div>
-        <a href="#"
-           class="toggle hidden md:flex w-full md:w-auto px-4 py-2 text-right bg-teal-900 hover:bg-teal-500 text-white md:rounded">Create
-            Account
-        </a>
-    </nav>
-        <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
-            <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
+<body class="flex  min-h-screen flex-col">
 
-            </main>
-        </div>
+    <x-navbar-component />
+    <div
+        class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
+        <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
+
+        </main>
+    </div>
 
 
-    </body>
+</body>
+
 </html>
