2ffc0afc4ef1fc72506601c29d6c15f104b1891f
mason.hows11 <mason.hows11@gmail.com>
2025-12-05 08:45:53 +0330

    navbar updated

--- resources/views/components/navbar-component.blade.php
+++ resources/views/components/navbar-component.blade.php
@@ -1,9 +1,15 @@
 <nav class="flex flex-wrap items-center justify-between p-3 bg-teal-200/20">
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-        class="size-6">
-        <path stroke-linecap="round" stroke-linejoin="round"
-            d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-    </svg>
+
+    @auth
+        <div class="flex">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+            </svg>
+            <span class="ml-2">{{ auth()->user()->name }}</span>
+        </div>
+    @endauth
 
     <div class="flex md:hidden">
         <button id="hamburger">
@@ -13,6 +19,7 @@
                 width="48" height="48" />
         </button>
     </div>
+
     <div
         class="toggle hidden w-full md:w-auto md:flex text-right text-bold mt-5 md:mt-0 border-t-2 border-teal-900 md:border-none">
         @if (Route::has('login'))
@@ -34,9 +41,10 @@
                 </a>
                 <form method="post" action="{{ route('logout') }}">
                     @csrf
-                    <button href="#" class="block md:inline-block text-teal-900
+                    <button href="#"
+                        class="block md:inline-block text-teal-900
                      hover:text-teal-500 px-3 py-3 border-b-2 border-teal-900 md:border-none">
-                       Logout
+                        Logout
                     </button>
                 </form>
             @else
@@ -61,3 +69,11 @@
     @endguest
 
 </nav>
+<script>
+    document.getElementById("hamburger").onclick = function toggleMenu() {
+        const navToggle = document.getElementsByClassName("toggle");
+        for (let i = 0; i < navToggle.length; i++) {
+            navToggle.item(i).classList.toggle("hidden");
+        }
+    };
+</script>
--- resources/views/tasks/master.blade.php
+++ resources/views/tasks/master.blade.php
@@ -15,13 +15,6 @@
 <x-navbar-component/>
 @yield('content')
 @yield('scripts')
-<script>
-    document.getElementById("hamburger").onclick = function toggleMenu() {
-        const navToggle = document.getElementsByClassName("toggle");
-        for (let i = 0; i < navToggle.length; i++) {
-            navToggle.item(i).classList.toggle("hidden");
-        }
-    };
-</script>
+
 </body>
 </html>
