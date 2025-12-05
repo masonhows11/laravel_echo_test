f6853dd7bc8da13b18fd159c1ad0a19c4cebe89f
mason.hows11 <mason.hows11@gmail.com>
2025-12-05 10:12:35 +0330

    nabbar items float updated

--- resources/views/components/navbar-component.blade.php
+++ resources/views/components/navbar-component.blade.php
@@ -1,26 +1,7 @@
 <nav class="flex flex-wrap items-center justify-between p-3 bg-teal-200/20">
 
-    @auth
-        <div class="flex">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                class="size-6">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-            </svg>
-            <span class="ml-2">{{ auth()->user()->name }}</span>
-        </div>
-    @endauth
-
-    <div class="flex md:hidden">
-        <button id="hamburger">
-            <img class="toggle block" src="https://img.icons8.com/fluent-systems-regular/2x/menu-squared-2.png"
-                width="48" height="48" />
-            <img class="toggle hidden" src="https://img.icons8.com/fluent-systems-regular/2x/close-window.png"
-                width="48" height="48" />
-        </button>
-    </div>
-
-    <div
+  
+  <div
         class="toggle hidden w-full md:w-auto md:flex text-right text-bold mt-5 md:mt-0 border-t-2 border-teal-900 md:border-none">
         @if (Route::has('login'))
             @auth
@@ -39,11 +20,11 @@
                 <a href="{{ url('/create') }}"
                     class="block md:inline-block text-teal-900 hover:text-teal-500 px-3 py-3 border-b-2 border-teal-900 md:border-none">Create
                 </a>
-                <form method="post" action="{{ route('logout') }}">
+                <form method="post" class="md:inline-block  border-b-2 border-teal-900 md:border-none" action="{{ route('logout') }}">
                     @csrf
-                    <button href="#"
+                    <button
                         class="block md:inline-block text-teal-900
-                     hover:text-teal-500 px-3 py-3 border-b-2 border-teal-900 md:border-none">
+                     hover:text-teal-500 px-3 py-3  border-teal-900 md:border-none">
                         Logout
                     </button>
                 </form>
@@ -59,8 +40,29 @@
             @endauth
         @endif
     </div>
+    <div class="flex md:hidden">
+        <button id="hamburger">
+            <img class="toggle block" src="https://img.icons8.com/fluent-systems-regular/2x/menu-squared-2.png"
+                width="48" height="48" />
+            <img class="toggle hidden" src="https://img.icons8.com/fluent-systems-regular/2x/close-window.png"
+                width="48" height="48" />
+        </button>
+    </div>
+
+      @auth
+        <div class="flex">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+            </svg>
+            <span class="ml-2">{{ auth()->user()->name }}</span>
+        </div>
+    @endauth
+
+  
     @guest
-        <a href="#"
+        <a href="{{ route('register') }}"
             class="toggle hidden md:flex w-full md:w-auto
                      px-4 py-2 text-right bg-teal-900
                     hover:bg-teal-500 text-white md:rounded">Create
